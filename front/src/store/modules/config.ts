017f7e9cf22370ad99ca99b3d2893749c6463ace
gaston duault <gas.duault@gmail.com>
2025-04-01 08:37:35 +0200

    Front: update firebase domaine

--- front/src/store/modules/config.ts
+++ front/src/store/modules/config.ts
@@ -1,4 +1,4 @@
 export default {
-  // API_URL: 'https://www.barique.fr/api',
-  API_URL: 'http://localhost:5001/api',
+  API_URL: 'https://www.barique.fr/api',
+  // API_URL: 'http://localhost:5001/api',
 } 
